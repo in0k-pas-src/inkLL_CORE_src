2aafa7a20c6c50a75050d24e6cc35295d4e863f7
in0k <val@kreit.ru>
2014-01-09 11:09:37 +0600

    косметика

--- inkLL_node.pas
+++ inkLL_node.pas
@@ -4,7 +4,7 @@ unit inkLL_node;
     *}
 {//..........................................................................///
 ///                                      _                                   ///
-///                      __ __ ___ ___ _| |___node->-                        ///
+///                      __ __ ___ ___ _| |___node->-<                       ///
 ///                     |  |  |   | . | . | -_| next->                       ///
 ///                     |  |_ |_|_|___|___|___|     -                        ///
 ///                     |____|___| Linked Lists     =                        ///
@@ -27,42 +27,48 @@ interface
 
 type
 
-  {УЗЕЛ связного списка }
+  {УЗЕЛ связного списка}
  rInkNodeLL=record
     next:pointer;  //< ссылка-указатель на следующий элемент очереди
   end;
 
-  {УКАЗАТЕЛЬ на УЗЕЛ связного списка }
+  {УКАЗАТЕЛЬ на УЗЕЛ связного списка}
  pInkNodeLL=^rInkNodeLL;
 
-type
+type //< кал Баки
 
   {"callBack" при УНИЧТОЖЕНИИ !!! СТАТИЧЕСКАЯ функция !!!
     @param(NODE указатель на УЗЕЛ списка [pInkNodeLL])  }
  fInkNodeLL_doDispose=procedure(const NODE:pointer);
-  {"callBack" при обходе очереди !!! метод ОБЪЕКТА-класса !!!
+  {"callBack" при УНИЧТОЖЕНИИ !!! метод ОБЪЕКТА-класса !!!
     @param(NODE указатель на УЗЕЛ очереди [pInkNodeLL]) }
  aInkNodeLL_doDispose=procedure(const NODE:pointer) of object;
 
   {"callBack" обработать Узел при обходе очереди !!! СТАТИЧЕСКАЯ функция !!!
-    @param (Data АДРЕС-памяти, некая инфа используемая при обходе)
-    @param (NODE это ссылка-указатель на УЗЕЛ очереди [pQueueNode])
-    @return(continue @true -- продолжить обход; @false -- ПРЕКРАТИТЬ)  }
+    @param (Context АДРЕС-памяти, некая инфа используемая при обходе)
+    @param (NODE    это ссылка-указатель на УЗЕЛ очереди [pInkNodeLL])
+    @return(@true -- продолжить обход; @false -- ПРЕКРАТИТЬ)  }
  fInkNodeLL_doProcess=function(const Context:pointer; const NODE:pointer):boolean;
   {"callBack" обработать Узел при обходе очереди !!! метод ОБЪЕКТА-класса !!!
-    @param (Data АДРЕС-памяти, некая инфа используемая при обходе)
-    @param (NODE это ссылка-указатель на УЗЕЛ очереди [pQueueNode])
+    @param (Context АДРЕС-памяти, некая инфа используемая при обходе)
+    @param (NODE    это ссылка-указатель на УЗЕЛ очереди [pInkNodeLL])
     @return(@true -- продолжить обход; @false -- ПРЕКРАТИТЬ)  }
- aInkNodeLL_doProcess=function(const Context:pointer; const NODE:pointer):boolean;
+ aInkNodeLL_doProcess=function(const Context:pointer; const NODE:pointer):boolean of object;
 
 type
 
- tInkLLNodeIndex=NativeUint;
- tInkLLNodeCount=tInkLLNodeIndex;
+ tInkLLNodeIndex=NativeUint;      //< тип для указания ИНДЕКСА элемента
+ tInkLLNodeCount=tInkLLNodeIndex; //< тип для указания КОЛ-ВА элементов
 
 const
+
+  {индекс, который счиаем как НЕ валидный (что-то типа -1)}
+ cInkNodeLL_NdfIndex=high(tInkLLNodeCount);
+
+  {максимально ВОЗМОЖНОЕ кол-во эл на данной платформе *}
  cInkNodeLL_MaxCount=high(tInkLLNodeCount);
- cInkNodeLL_NdfIndex=cInkNodeLL_MaxCount;
+
+  {максимально ВОЗМОЖНЫЙ индекс элемента}
  cInkNodeLL_MaxIndex=cInkNodeLL_NdfIndex-1;
 
 function  InkNodeLL_getNext(const node:pointer):pointer;                        {$ifdef _INLINE_} inline; {$endif}
@@ -83,8 +89,4 @@ begin
 end;
 
 end.
-
-{//---[ in0k © 29.05.2013 ]--------------------------------------------------///
-    отпочковалась откуда-то
-///--------------------------------------------------------------------------//}
 
